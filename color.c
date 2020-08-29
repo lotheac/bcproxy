c0dc045e1cd4aa5bc4bc227e4e2823c944a10521
Lauri Tirkkonen <lauri@hacktheplanet.fi>
2020-08-29 22:38:34 +0300

    revert back to CSI 38 ; Pr ; Pg ; Pb for 24bit color
    previous commit turned out to be too optimistic; the form
    CSI 38 : Pi : Pr : Pb : Pg does not seem to work with eg. st 0.8.4 or
    alacritty 0.3.3. XTerm(351) and tmux openbsd-6.7 do understand it, but
    in the interest if compatibility, let's stick with the form that most
    terminals implement, even if incorrect by standard.

--- color.c
+++ color.c
@@ -7,10 +7,7 @@
  * the direct color control sequence we generate contains, at maximum:
  *     2 fixed bytes: ESC [
  *     2 bytes for foreground/background selection ("38" or "48")
- *     3 fixed bytes: : 2 :
- *     2 fixed bytes: 0 :
- *       - this is Pi, the "color space identifier", which is ignored by
- *       xterm
+ *     3 fixed bytes: ; 2 ;
  *     3 bytes for Pr, the red color value
  *     1 byte: :
  *     3 bytes for Pg, the green color value
@@ -18,12 +15,12 @@
  *     3 bytes for Pb, the blue color value
  *     2 bytes: m NUL
  */
-static char colorbuf[2+2+3+2+3+1+3+1+3+2];
+static char colorbuf[2+2+3+3+1+3+1+3+2];
 
 char *
 colorstr(bool foreground, uint8_t r, uint8_t g, uint8_t b)
 {
-	snprintf(colorbuf, sizeof(colorbuf), "\x1b[%s:2:0:%hhu:%hhu:%hhum",
+	snprintf(colorbuf, sizeof(colorbuf), "\x1b[%s;2;%hhu;%hhu;%hhum",
 	    foreground ? "38" : "48", r, g, b);
 	return colorbuf;
 }
