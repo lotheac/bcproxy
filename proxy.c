5ba1f039b5d0bd0d8d17953d08e310ed233b6a31
Risto Hietala <risto@hietala.org>
2021-08-24 20:39:26 +0300

    fix indentation

--- proxy.c
+++ proxy.c
@@ -170,8 +170,8 @@ on_close(struct bc_parser *parser)
 	case 53: /* exp */
 	case 54: /* player status */
 	case 60: /* player location */
-    break;
-  case 61: /* party place */
+		break;
+	case 61: /* party place */
 		buffer_append_str(st->obuf, MARKER "partyplace ");
 		buffer_append_buf(st->obuf, st->tmpbuf);
 		buffer_append_str(st->obuf, "\n");
